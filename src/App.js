759b16b3ab58b6e64cf5a735f1daaec25fe0d14c
Kendra Keith <kendra.d.keith@gmail.com>
2018-05-11 19:51:19 -0400

    Passing filtered and sorted array

--- src/App.js
+++ src/App.js
@@ -12,17 +12,46 @@ class App extends Component {
     axios
       .get("https://h93rvy36y7.execute-api.us-east-1.amazonaws.com/teams")
       .then(response => {
-        console.log(response);
+        //Filter by state
+        var stateFilter = response.data.filter(data => data.state === "VA");
 
-        const newNames = response.data.map(data => {
-          return {
-            name: data.name
-          };
-        });
-        console.log(newNames);
+        //Filter by role
+        var roleFilter = [];
+        for (var i = 0; i < stateFilter.length; i++) {
+          roleFilter.push(
+            stateFilter[i].members.filter(
+              members => members.role === "Software Engineer"
+            )
+          );
+        }
+        var flatArray = [].concat.apply([], roleFilter);
 
+        //Sort by last then first name
+        function firstCompare(a, b) {
+          if (a.firstName < b.firstName) return -1;
+          if (a.firstName > b.firstName) return 1;
+          return 0;
+        }
+        flatArray.sort(firstCompare);
+
+        function lastCompare(a, b) {
+          if (a.lastName < b.lastName) return -1;
+          if (a.lastName > b.lastName) return 1;
+          return 0;
+        }
+        flatArray.sort(lastCompare);
+
+        //Concat first and last names
+        var fullNames = [];
+        for (var j = 0; j < flatArray.length; j++) {
+          fullNames.push({
+            name: flatArray[j].firstName + " " + flatArray[j].lastName
+          });
+        }
+
+        //Set state
         const newState = Object.assign({}, this.state, {
-          names: newNames
+          names: fullNames
         });
 
         this.setState(newState);
