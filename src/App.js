86f82199d1ecb6563f2ec5122e47fec8f0a19cad
Kendra Keith <kendra.d.keith@gmail.com>
2018-05-11 10:52:19 -0400

    Add NameList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 
-import Name from "./components/Name/Name.js";
+import NameList from "./components/NameList/NameList.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Name />
+        <NameList />
       </div>
     );
   }
---
+++ src/components/NameList/NameList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./name-list.css";
+
+import Name from "../Name/Name.js";
+
+function NameList(props) {
+  return (
+    <div className="name-list">
+      <Name />
+    </div>
+  );
+}
+
+export default NameList;
