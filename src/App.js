c11f5ef11adbfe02c40e1013fa71ae18108c0f3e
Kendra Keith <kendra.d.keith@gmail.com>
2018-05-11 10:41:58 -0400

    Add Name component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 
+import Name from "./components/Name/Name.js";
+
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Name />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Name/Name.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./name.css";
+
+function Name(props) {
+  return (
+    <div className="name">
+      <span>First Last</span>
+    </div>
+  );
+}
+
+export default Name;
